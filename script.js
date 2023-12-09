34f244153022d19c02914325a492b36dcf119a02
Sean Uz <cuz3@gatech.edu>
2023-12-09 15:56:34 -0500

    Add conversion functions

--- script.js
+++ script.js
@@ -2,6 +2,47 @@ const ROCK = 0;
 const PAPER = 1;
 const SCISSORS = 2;
 
+
+function getNumEquiv(choice) {
+    choice = String(choice).toLowerCase;
+    
+    switch (choice) {
+        case "rock":
+            return ROCK;
+            break;
+
+        case "paper":
+            return PAPER;
+            break;
+
+        case "scissors":
+            return SCISSORS;
+            break;
+    }
+}
+
+
+function getStrEquiv(choice) {
+    switch (choice) {
+        case ROCK:
+            return "rock";
+            break;
+        
+        case PAPER:
+            return "paper";
+            break;
+        
+        case SCISSORS:
+            return "scissors";
+    }
+}
+
+
 function getComputerChoice() {
     return Math.floor(Math.random() * 3);
+}
+
+
+function playRound() {
+
 }
